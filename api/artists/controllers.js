1cc98af3fb0fce2a924f43b2cba9cbc2cdbed89e
Mykola Marenich <marenich.mykola@gmail.com>
2020-06-04 20:52:57 +0200

    artists

--- api/artists/controllers.js
+++ api/artists/controllers.js
@@ -14,14 +14,64 @@ const controllers = {
       res.json(rows)
     });
   },
-  getOne: (req, res) => { },
+  getOne: (req, res) => {
+    const id = req.params.id;
+    const sql = `select * from artists where artistid is '${id}'`;
+
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+
+      res.json(rows)
+    });
+   },
   create: (req, res) => {
     // read row data from body
+    const name = req.body.Name;
+    
+    const sql = `insert into artists (name) values ('${name}')`;
+
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+
+      res.json(rows)
+    });
   },
   update: (req, res) => {
     // read row data from body
+    const id = parseInt(req.params.id);
+    const name = req.body.Name;
+
+    const sql = `update artists set name = '${name}' where artistid is '${id}'`;
+
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+
+      res.json(rows)
+    });
   },
-  delete: (req, res) => { }
+  delete: (req, res) => {
+    const id = req.params.id;
+
+    const sql = `delete from artists where artistid is '${id}'`;
+
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+
+      res.send('Album deleted')
+    });
+   }
 }
 
 module.exports = controllers;
